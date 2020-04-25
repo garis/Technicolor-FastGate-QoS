fee0346910f40f310c6d793df3cdeea86b391a78
rgasparini <cinema4dgcm057ax>
2020-04-25 22:24:17 +0200

    qos file description

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # Technicolor-FastGate-QoS
+
+What can it do:
+* run multiple torrents client in multiple PCs with no upload limits
+* run multiple upload speedtests in multiple PCs
+* run a teamspeak session with no latency issue
+* ...all at the same time
+
+Note that this setup will manage only upload traffic since we can control each packet that will go on the internet link. The download is out of our control since we can only receive and we have no control of that
+
+The file named 'qos' has 6 sections:
+
+* `config label` types of traffic
+* `config class` single QoS class
+* `config classgroup` queue disciplines between classes
+* `config interface` logical interfaces
+* `config device` physical interfaces
+* `config rule` QoS matching rules
+
+Each `rule` match some specified traffic and marks it with a `label`
+
+The `label` is used as trafficid and is a part of a `class` and can be think as a queue.
+
+Each `class` is then part of a `classgroup` that form the QoS scheduling. 
+
+A physical interface that need a QoS mechanism or an interface is linked with a `device` or a `interface` and linked to a `classgroup`
+ 
+The example config file called `qos` is structured in the following parts:
+
++ 4 `label`:
+  
+  +  `LowP` low priority traffic identified with `trafficid 1`
+  +  `NormalP` normal priority traffic identified with `trafficid 2`
+  +  `HighP` high priority traffic identified with `trafficid 3`
+  +  `VeryHighP` very high priority traffic identified with `trafficid4`
+
+* 8 `class`: 4 for WAN/Internet traffic (`W_Q0`,`W_Q1`,`W_Q2`,`W_Q3`) and 4 for LAN/local (`L_Q0`,`L_Q1`,`L_Q2`,`L_Q3` for local traffic):
+  
+  * `W_Q0` lowest priority (=1) class linked to `LowP` with 0 minimum bit rate e 0 weight
+  * `W_Q1` normal priority (=2) class linked to `NormalP` with a bit more weight and minimum bandwith
+  * `W_Q2` high priority (=3) class linked to `HighP` with even a bit more weight and same minimum bandwith
+  * `W_Q3` very high priority (=4) class linked to `VeryHighP` with a lot more weight and same minimum bandwith
+  * `L_Q0`,`L_Q1`,`L_Q2`,`L_Q3` same as `W_Q0`,`W_Q1`,`W_Q2`,`W_Q3`
+
+* 2 `classgroup`: one for WAN traffic and another for LAN traffic
+  * `TO_WAN` uses the classes `W_Q0`,`W_Q1`,`W_Q2`,`W_Q3` with theirs options (priority, weight, label, etc) and uses `W_Q2` as the default traffic with the policy `sp_wrr` that stand for Strict Priority and Weighted Round Robin
+  * `TO_LAN` same as `TO_WAN` but uses the classes `L_Q0`,`L_Q1`,`L_Q2`,`L_Q3`
+
+* 2 `interface`:
+  * `wan` uses the `classgroup` referred as `TO_WAN` and is enabled
+  * same as `wan` but for `TO_LAN`
+
+* 2 `device` that do the same thing as the `interface`, it's unclear the one needed...added both just to be sure
+  
++ some `rule` to match SIP, ICMP, HTTP, HTTPS and torrent traffic and apply a somewhat good QoS.
+
+The reasoning behind the rules are:
++ Everything is **high priority** traffic **BUT**:
+  + SIP, ICMP and UDP is **very high priority** traffic
+  + HTTP and HTTPS are **normal priority** traffic
+  + Torrent is **low priority** traffic
+
+This should cover a lot of cases with not too much rules... at least thats the hope.
+
+## Details
+
+#TODO#
---
+++ script.sh
@@ -0,0 +1,311 @@
+#!/bin/sh
+
+#######################################################
+# npshaper v0.1
+# https://forum.dd-wrt.com/phpBB2/viewtopic.php?p=692003
+#######################################################
+
+# MAYBE (not sure if it's working)
+# uci set wireless.radio_5G.tx_power_adjust='4'
+# uci set wireless.radio_5G.tx_power_overrule_reg='1'
+# uci commit
+# uci show|grep tx
+# then disable and enable wifi on the web ui
+
+
+# Wan link download speed in Kbits (set to 80%-90% of link capacity; 6000 -> 4800)
+DOWNLOAD=33000
+# Wan link upload speed in Kbits (set to 80%-90% of link capacity; 600 -> 480)
+UPLOAD=8700
+
+# Download burst size in Kbytes
+D_BURST=1000
+# Upload burst size in Kbytes
+U_BURST=100
+
+#
+# Ports used by the torrent services
+#
+HOMESERVER_BITTORRENT_PORT1=51413
+HOMESERVER_BITTORRENT_PORT2=51414
+
+#######################################################
+
+WAN=vlan_ptm0
+PTM=ptm0
+LAN=br-lan
+
+DEBUG=0
+
+if [ "$1" = "start" ]
+then
+
+   echo "Starting..."
+   
+  [ $DEBUG -eq 1 ] && insmod ipt_LOG >&- 2>&-
+
+
+   # Remove previous settings
+   
+   qos -6 stop
+   qos -4 stop
+   iptables -t mangle -F
+   tc qdisc del dev $WAN root >&- 2>&- 
+   tc qdisc del dev $PTM root >&- 2>&- 
+   tc qdisc del dev $LAN root >&- 2>&-   
+   
+   ##### WAN #####
+   echo "Setting up Wan interface traffic classes..."
+   tc qdisc add dev $WAN root handle 1: htb
+     tc class add dev $WAN parent 1: classid 1:1 htb rate ${UPLOAD}kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k
+       tc class add dev $WAN parent 1:1 classid 1:10 htb rate $(($UPLOAD*45/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 0
+       tc class add dev $WAN parent 1:1 classid 1:20 htb rate $(($UPLOAD*25/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 1
+       tc class add dev $WAN parent 1:1 classid 1:30 htb rate $(($UPLOAD*15/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 2
+       tc class add dev $WAN parent 1:1 classid 1:40 htb rate $(($UPLOAD*8/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 3
+       
+       #echo "Setting up Wan interface traffic htb queues..."
+       #tc class add dev $WAN parent 1:1 classid 1:2 htb rate 2000kbit ceil ${UPLOAD}kbit burst 300k cburst 300k prio 0
+       #tc class add dev $WAN parent 1:1 classid 1:3 htb rate 2000kbit ceil ${UPLOAD}kbit burst 1000k cburst 1000k prio 1
+       #tc class add dev $WAN parent 1:1 classid 1:4 htb rate 3000kbit ceil ${UPLOAD}kbit burst 500k cburst 500k prio 2
+       #tc class add dev $WAN parent 1:1 classid 1:50 htb rate 500kbit ceil ${UPLOAD}kbit burst 100k cburst 100k prio 3
+        
+       #echo "Setting up Wan interface traffic sfq queues..."
+       #tc qdisc add dev $WAN parent 1:2 handle 20: sfq perturb 10
+       #tc qdisc add dev $WAN parent 1:3 handle 30: sfq perturb 10
+       #tc qdisc add dev $WAN parent 1:4 handle 40: sfq perturb 10
+   
+   #echo "Setting up Wan interface traffic flowids..."    
+   #tc filter add dev $WAN parent 1: prio 1 protocol ip handle 1 fw flowid 1:20
+   #tc filter add dev $WAN parent 1: prio 2 protocol ip handle 2 fw flowid 1:30
+   #tc filter add dev $WAN parent 1: prio 3 protocol ip handle 3 fw flowid 1:40
+   #tc filter add dev $WAN parent 1: prio 4 protocol ip handle 4 fw flowid 1:50
+   tc filter add dev $WAN parent 1: prio 1 protocol ip handle 1 fw flowid 1:10
+   tc filter add dev $WAN parent 1: prio 2 protocol ip handle 2 fw flowid 1:20
+   tc filter add dev $WAN parent 1: prio 3 protocol ip handle 3 fw flowid 1:30
+   tc filter add dev $WAN parent 1: prio 4 protocol ip handle 4 fw flowid 1:40
+
+   ##### PTM #####
+   echo "Setting up Ptm interface traffic classes..."
+   tc qdisc add dev $PTM root handle 1: htb
+     tc class add dev $PTM parent 1: classid 1:1 htb rate ${UPLOAD}kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k
+       tc class add dev $PTM parent 1:1 classid 1:10 htb rate $(($UPLOAD*45/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 0
+       tc class add dev $PTM parent 1:1 classid 1:20 htb rate $(($UPLOAD*25/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 1
+       tc class add dev $PTM parent 1:1 classid 1:30 htb rate $(($UPLOAD*15/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 2
+       tc class add dev $PTM parent 1:1 classid 1:40 htb rate $(($UPLOAD*8/100))kbit ceil ${UPLOAD}kbit burst ${U_BURST}k cburst ${U_BURST}k prio 3
+
+   tc filter add dev $PTM parent 1: prio 1 protocol ip handle 1 fw flowid 1:10
+   tc filter add dev $PTM parent 1: prio 2 protocol ip handle 2 fw flowid 1:20
+   tc filter add dev $PTM parent 1: prio 3 protocol ip handle 3 fw flowid 1:30
+   tc filter add dev $PTM parent 1: prio 4 protocol ip handle 4 fw flowid 1:40   
+   
+   ##### LAN #####
+   echo "Setting up Lan interface traffic classes..."
+   tc qdisc add dev $LAN root handle 1: htb
+     tc class add dev $LAN parent 1: classid 1:1 htb rate ${DOWNLOAD}kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k
+       #tc class add dev $LAN parent 1:1 classid 1:10 htb rate $(($DOWNLOAD*5/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 0
+       #tc class add dev $LAN parent 1:1 classid 1:20 htb rate $(($DOWNLOAD*3/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 1
+       #tc class add dev $LAN parent 1:1 classid 1:30 htb rate $(($DOWNLOAD*2/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 2
+       #tc class add dev $LAN parent 1:1 classid 1:40 htb rate $(($DOWNLOAD*1/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 3
+       
+       tc class add dev $LAN parent 1:1 classid 1:10 htb rate $(($DOWNLOAD*5/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 0       
+       tc class add dev $LAN parent 1:1 classid 1:20 htb rate $(($DOWNLOAD*3/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 1       
+       tc class add dev $LAN parent 1:1 classid 1:30 htb rate $(($DOWNLOAD*2/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 2       
+       tc class add dev $LAN parent 1:1 classid 1:40 htb rate $(($DOWNLOAD*1/10))kbit ceil ${DOWNLOAD}kbit burst ${D_BURST}k cburst ${D_BURST}k prio 3       
+   
+   tc filter add dev $LAN parent 1: prio 1 protocol ip handle 1 fw flowid 1:10
+   tc filter add dev $LAN parent 1: prio 2 protocol ip handle 2 fw flowid 1:20
+   tc filter add dev $LAN parent 1: prio 3 protocol ip handle 3 fw flowid 1:30
+   tc filter add dev $LAN parent 1: prio 4 protocol ip handle 4 fw flowid 1:40
+   
+   
+   
+   ######################################## MARK CHAIN ##################################################
+   
+   echo "Setting up classification chains..."
+   
+   # Remove previous settings
+   iptables -t mangle -F
+   iptables -t mangle -X
+   
+   # Wan ('upload' traffic) classification chain
+   iptables -t mangle -N wan_mark_chain
+   iptables -t mangle -A POSTROUTING -o $WAN -j wan_mark_chain
+   
+   # Ptm ('---' traffic) classification chain
+   iptables -t mangle -N ptm_mark_chain
+   iptables -t mangle -A POSTROUTING -o $PTM -j ptm_mark_chain
+   
+   # Lan ('download' traffic) classification chain
+   iptables -t mangle -N lan_mark_chain
+   iptables -t mangle -A POSTROUTING -o $LAN -j lan_mark_chain
+   
+   # Restore any saved connection mark (connection already marked and tracked)
+   iptables -t mangle -A wan_mark_chain -j CONNMARK --restore-mark
+   iptables -t mangle -A ptm_mark_chain -j CONNMARK --restore-mark
+   iptables -t mangle -A lan_mark_chain -j CONNMARK --restore-mark
+   
+   ### RULES BEGIN #####################################
+   
+   # DNS (outgoing) and VoIP queries - Express
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --dport 53 -j MARK --set-mark 1
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --dport 5060 -j MARK --set-mark 1
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --dport 50601 -j MARK --set-mark 1
+   
+   # HTTP and HTTPS traffic Bulk
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --match multiport --sport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --match multiport --dport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --match multiport --sport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --match multiport --dport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --match multiport --sport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --match multiport --dport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --match multiport --sport 80,443 -j MARK --set-mark 3
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --match multiport --dport 80,443 -j MARK --set-mark 3
+   
+   # Torrent traffic - LowBulk
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --sport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --dport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --sport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --dport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --sport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --dport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --sport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --dport $HOMESERVER_BITTORRENT_PORT1 -j MARK --set-mark 4
+   
+   
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --sport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p tcp --dport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --sport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -p udp --dport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --sport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p tcp --dport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --sport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -p udp --dport $HOMESERVER_BITTORRENT_PORT2 -j MARK --set-mark 4
+   
+   ### RULES END #####################################
+   
+   # Save mark so we track the full connection
+   iptables -t mangle -A wan_mark_chain -j CONNMARK --save-mark
+   iptables -t mangle -A ptm_mark_chain -j CONNMARK --save-mark
+   iptables -t mangle -A lan_mark_chain -j CONNMARK --save-mark
+   
+   # ACK packets and suck (connection control) - Express
+   iptables -t mangle -A wan_mark_chain -p tcp -m length --length :128 --tcp-flags SYN,RST,ACK ACK -j MARK --set-mark 1
+   iptables -t mangle -A lan_mark_chain -p tcp -m length --length :128 --tcp-flags SYN,RST,ACK ACK -j MARK --set-mark 1
+   
+   # ICMP (ping and such) - Express
+   iptables -t mangle -A wan_mark_chain -p icmp -j MARK --set-mark 1
+   iptables -t mangle -A lan_mark_chain -p icmp -j MARK --set-mark 1
+   
+   # TOS Minimize-Delay - Express
+   iptables -t mangle -A wan_mark_chain -m tos --tos Minimize-Delay -j MARK --set-mark 1
+   iptables -t mangle -A lan_mark_chain -m tos --tos Minimize-Delay -j MARK --set-mark 1
+   
+   # Default (anything else) - Normal
+   iptables -t mangle -A wan_mark_chain -m mark --mark 0 -j MARK --set-mark 2
+   iptables -t mangle -A lan_mark_chain -m mark --mark 0 -j MARK --set-mark 2
+   
+   
+   #### DO WE NEED THIS ALSO? #####
+   #Chain POSTROUTING (policy ACCEPT 761 packets, 292K bytes)
+   # spkts bytes target     prot opt in     out     source               destination         
+   # 370 31460 wan_mark_chain  all  --  any    vlan_ptm0  anywhere             anywhere            
+   # 0     0 ptm_mark_chain  all  --  any    ptm0    anywhere             anywhere            
+   # 406  261K lan_mark_chain  all  --  any    br-lan  anywhere             anywhere   
+   
+   
+   ######################################################################################################
+   
+   echo "Setting up debugging..."
+   
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A wan_mark_chain -m mark --mark 1 -j LOG --log-prefix wan_qos_express::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A wan_mark_chain -m mark --mark 2 -j LOG --log-prefix wan_qos_normal::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A wan_mark_chain -m mark --mark 3 -j LOG --log-prefix wan_qos_bulk::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A wan_mark_chain -m mark --mark 4 -j LOG --log-prefix wan_qos_lowbulk::
+   
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A ptm_mark_chain -m mark --mark 1 -j LOG --log-prefix wan_qos_express::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A ptm_mark_chain -m mark --mark 2 -j LOG --log-prefix wan_qos_normal::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A ptm_mark_chain -m mark --mark 3 -j LOG --log-prefix wan_qos_bulk::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A ptm_mark_chain -m mark --mark 4 -j LOG --log-prefix wan_qos_lowbulk::
+   
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A lan_mark_chain -m mark --mark 1 -j LOG --log-prefix lan_qos_express::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A lan_mark_chain -m mark --mark 2 -j LOG --log-prefix lan_qos_normal::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A lan_mark_chain -m mark --mark 3 -j LOG --log-prefix lan_qos_bulk::
+   [ $DEBUG -eq 1 ] && iptables -t mangle -A lan_mark_chain -m mark --mark 4 -j LOG --log-prefix lan_qos_lowbulk::
+   
+   echo "Setting up accounting..."
+   
+   iptables -t mangle -A wan_mark_chain -m mark --mark 1 -j RETURN
+   iptables -t mangle -A wan_mark_chain -m mark --mark 2 -j RETURN
+   iptables -t mangle -A wan_mark_chain -m mark --mark 3 -j RETURN
+   iptables -t mangle -A wan_mark_chain -m mark --mark 4 -j RETURN
+   
+   iptables -t mangle -A ptm_mark_chain -m mark --mark 1 -j RETURN   
+   iptables -t mangle -A ptm_mark_chain -m mark --mark 2 -j RETURN   
+   iptables -t mangle -A ptm_mark_chain -m mark --mark 3 -j RETURN   
+   iptables -t mangle -A ptm_mark_chain -m mark --mark 4 -j RETURN   
+   
+   iptables -t mangle -A lan_mark_chain -m mark --mark 1 -j RETURN
+   iptables -t mangle -A lan_mark_chain -m mark --mark 2 -j RETURN
+   iptables -t mangle -A lan_mark_chain -m mark --mark 3 -j RETURN
+   iptables -t mangle -A lan_mark_chain -m mark --mark 4 -j RETURN
+   
+   echo "...OK, all done."
+
+fi
+
+########################################
+
+if [ "$1" = "status" ]
+then
+   echo "--- Current status ---"
+   echo "--- WAN (Upload) ---"
+
+   tc -s qdisc ls dev $WAN
+   tc -s class ls dev $WAN
+   echo ""
+   echo "--- PTM (---) ---"
+
+   tc -s qdisc ls dev $PTM
+   tc -s class ls dev $PTM
+   echo ""
+   echo "--- LAN (Download) ---"
+
+   tc -s qdisc ls dev $LAN
+   tc -s class ls dev $LAN
+   echo ""
+   echo "--- Classification chains ---"
+   iptables -L -v -t mangle
+   echo ""
+fi
+
+if [ "$1" = "stats" ]
+then
+   LAN_EXPRESS_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x1" | head -n 1 | awk '{print $1}'`
+   LAN_NORMAL_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x2" | head -n 1 | awk '{print $1}'`
+   LAN_BULK_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x3" | head -n 1 | awk '{print $1}'`
+   LAN_LOWBULK_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x3" | head -n 1 | awk '{print $1}'`
+   LAN_EXPRESS_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x1" | head -n 1 | awk '{print $2}'`
+   LAN_NORMAL_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x2" | head -n 1 | awk '{print $2}'`
+   LAN_BULK_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x3" | head -n 1 | awk '{print $2}'`
+   LAN_LOWBULK_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x4" | head -n 1 | awk '{print $2}'`
+   
+   WAN_EXPRESS_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x1" | tail -n 1 | awk '{print $1}'`
+   WAN_NORMAL_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x2" | tail -n 1 | awk '{print $1}'`
+   WAN_BULK_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x3" | tail -n 1 | awk '{print $1}'`
+   WAN_LOWBULK_PACKETS=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x4" | tail -n 1 | awk '{print $1}'`
+   WAN_EXPRESS_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x1" | tail -n 1 | awk '{print $2}'`
+   WAN_NORMAL_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x2" | tail -n 1 | awk '{print $2}'`
+   WAN_BULK_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x3" | tail -n 1 | awk '{print $2}'`
+   WAN_LOWBULK_BYTES=`iptables -L -v -n -t mangle | grep "RETURN" | grep "match 0x4" | tail -n 1 | awk '{print $2}'`
+   
+   echo "Traffic stats:"
+   echo "D/U Class    Packets Bytes"
+   echo "D   Express  $LAN_EXPRESS_PACKETS $LAN_EXPRESS_BYTES"
+   echo "D   Normal   $LAN_NORMAL_PACKETS $LAN_NORMAL_BYTES"
+   echo "D   Bulk     $LAN_BULK_PACKETS $LAN_BULK_BYTES"
+   echo "D   LowBulk  $LAN_LOWBULK_PACKETS $LAN_LOWBULK_BYTES"
+   echo "U   Express  $WAN_EXPRESS_PACKETS $WAN_EXPRESS_BYTES"
+   echo "U   Normal   $WAN_NORMAL_PACKETS $WAN_NORMAL_BYTES"
+   echo "U   Bulk     $WAN_BULK_PACKETS $WAN_BULK_BYTES"
+   echo "U   LowBulk  $WAN_LOWBULK_PACKETS $WAN_LOWBULK_BYTES"
+fi
